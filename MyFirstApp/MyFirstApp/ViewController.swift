78d6e37e588b4a91cffb6a6aaf1a9310db288e99
Ahmet Rıdvan Ordulu <ahmetridvanordulu@icloud.com>
2025-10-05 22:44:30 +0300

    sd
    sd

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Ahmet Rıdvan Ordulu on 10/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
