d815e565d017d7e1795496300a52748472194729
Ahmet Rıdvan Ordulu <ahmetridvanordulu@icloud.com>
2025-10-09 00:00:42 +0300

    Add variable and type examples to Contents.swift
    Expanded Contents.swift with examples of variables, constants, string operations, integer and double arithmetic, and boolean usage for demonstration purposes.

--- playground/Variables.playground/Contents.swift
+++ playground/Variables.playground/Contents.swift
@@ -1,9 +1,40 @@
 import UIKit
+//Variables & Constants
+
+ 
+// camelCase
+
+var studentName = "Ahmet"
+print(studentName)
+var studentSurname  = "Ordulu"
+studentName = "Emir"
+print(studentName)
 
 
+let pi = 3.14
+let userAge = 50.0
 
-print(3+2)
+pi * userAge
+
+//String
+var exampleString = "Hi"
+exampleString.append("zooz")
+exampleString.lowercased()
+exampleString.uppercased()
+
+// integer
+
+let number: Int = 10
+var number2: Int = 2
+
+number / number2
+
+//double
+let numberx: Double = 10.0
+let numbery: Double = 2.0
 
 
-4*400
+//boolean
 
+var myBoolean: Bool = false
+myBoolean = true
